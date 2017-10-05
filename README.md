081b93ddaefc9fb5741bb7b22dd1cfca0028d14c
david morse <davidbryanmarshall@gmail.com>
2017-10-04 21:39:11 -0700

    html/css layout set for game

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Rock Paper Scissors Multiplayer
 
-Week Four Homework Part 1: [RPS Challenge Assignment](http://ucb.bootcampcontent.com/UCB-Coding-Bootcamp/09-11-2017-UCB-Class-Repository-FSF-FT/blob/master/04-week/homework/part-1/Instructions/Homework_RPS_Activity_Challenge.md).
+Week Four Homework Part 1: [Firebase Assignment](http://ucb.bootcampcontent.com/UCB-Coding-Bootcamp/09-11-2017-UCB-Class-Repository-FSF-FT/blob/master/04-week/homework/part-1/Instructions/Homework_RPS_Activity_Challenge.md).
 
 ### No Prerequisites, Installation, or Testing Instructions required
 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,31 @@
 
 $(document).ready(function() {
 
-  
+  // Initialize Firebase
+  var config = {
+    apiKey: 'AIzaSyCEC51xbY107uDWNawFfhprL6fIvr_f-h4',
+    authDomain: 'rps-multiplayer-9d6f5.firebaseapp.com',
+    databaseURL: 'https://rps-multiplayer-9d6f5.firebaseio.com',
+    projectId: 'rps-multiplayer-9d6f5',
+    storageBucket: 'rps-multiplayer-9d6f5.appspot.com',
+    messagingSenderId: '102143919957'
+  };
+  firebase.initializeApp(config);
+
+  var database =  firebase.database();
+
+  var connectionsRef = database.ref('/connections');
+
+  var connectedRef = database.ref('.info/connected');
+
+  connectedRef.on("value", function(snap) {
+    // If they are connected..
+    if (snap.val()) {
+      // Add user to the connections list.
+      var con = connectionsRef.push('userconnect');
+      // Remove user from the connection list when they disconnect.
+      con.onDisconnect().remove();
+    }
+  });
 
 });
